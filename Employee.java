213fe95d4e99e3e43712ca3d19151a475f185272
proximityguy <rajamanish2513@gmail.com>
2026-06-12 20:31:39 +0530

    error fix

--- Employee.java
+++ Employee.java
@@ -3,12 +3,7 @@ class Employee {
 		String name = "Krishna Kumar";
 		String post = "Sr. Developer";
 		int age = 32;
-<<<<<<< HEAD
 		double salary = 93000;
-=======
-		double salary = 89000;
 		string location = "chennai";
->>>>>>> 6ce190fbe77aabc2037fd2e8d9bf01706322577c
 	}
 }
-
