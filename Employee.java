a5f815907e11dacb602af0ae44954087d8863c14
proximityguy <rajamanish2513@gmail.com>
2026-06-12 19:33:43 +0530

    new changes

--- Employee.java
+++ Employee.java
@@ -1,1 +1,9 @@
-created the file
+class Employee {
+	public static void main(String[] args){
+		String name = "Krishna Kumar";
+		String post = "Sr. Developer";
+		int age = 32;
+		double salary = 89000;
+	}
+}
+
